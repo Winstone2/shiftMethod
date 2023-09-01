4289d8416c28f571f27475737da8462a7d294bcf
Winstone <you@example.com>
2023-09-01 23:23:27 +0300

    first commit

---
+++ shiftMethods.js
@@ -0,0 +1,11 @@
+// Create an array
+let fruits = ['apple', 'banana', 'cherry', 'date'];
+
+// Use the shift() method to remove and return the first element
+let removedFruit = fruits.shift();
+
+// Display the removed element
+console.log('Removed fruit:', removedFruit); // Output: Removed fruit: apple
+
+// Display the modified array after removing the first element
+console.log('Modified array:', fruits); // Output: Modified array: ['banana', 'cherry', 'date']
